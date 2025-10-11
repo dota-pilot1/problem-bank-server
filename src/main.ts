25bc6fe3b859e18e7ffeb1e8865e93821e476c45
oh hyun seok <hyunsokstar@gmail.com>
2025-10-11 16:00:07 +0900

    문제 은행 cors 설정

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,44 @@ async function bootstrap() {
 
   app.enableCors({
     origin: [
+      // Local development (다양한 포트)
       'http://localhost:5173',
       'http://localhost:5174',
       'http://localhost:5175',
+      'http://localhost:5176',
+      'http://localhost:3000',
+      'http://127.0.0.1:5173',
+      'http://127.0.0.1:5174',
+      'http://127.0.0.1:5175',
+      // Production IPs
+      'http://3.36.184.159',
+      'https://3.36.184.159',
+      'http://43.200.241.26',
+      'https://43.200.241.26',
+      'http://43.201.126.232',
+      'https://43.201.126.232',
+      // Production domains
+      'http://total-callbot.cloud',
+      'https://total-callbot.cloud',
+      'http://www.total-callbot.cloud',
+      'https://www.total-callbot.cloud',
+      'http://realtime-englsih-trainer.co.kr',
+      'https://realtime-englsih-trainer.co.kr',
+      'http://www.realtime-englsih-trainer.co.kr',
+      'https://www.realtime-englsih-trainer.co.kr',
+      'http://api.realtime-englsih-trainer.co.kr',
+      'https://api.realtime-englsih-trainer.co.kr',
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
-    allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
+    allowedHeaders: [
+      'Content-Type',
+      'Authorization',
+      'Accept',
+      'X-Requested-With',
+    ],
+    exposedHeaders: ['Content-Length', 'Content-Type'],
+    maxAge: 3600,
   });
 
   await app.listen(process.env.PORT ?? 8090);
